d4d4f14b4fcfab6424fd0cbae7e883c39ed75df0
Tholgan <marty.florian@gmail.com>
2020-06-29 14:38:23 +0200

    Startup class added and moved main method in it.

---
+++ csharp/Tasks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    class Startup
+    {
+        public static void Main(string[] args)
+        {
+            new TaskList(new RealConsole()).Run();
+        }
+    }
+}
