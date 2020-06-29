007bbb2032e0c7fec20c76fbff2b6be8ede71521
Tholgan <marty.florian@gmail.com>
2020-06-29 17:14:28 +0200

    Supression of Blocking Stream and added StreamOutput

---
+++ csharp/Tasks.Tests/StreamOutput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Tasks
+{
+	public class StreamOutput
+	{
+        private StringWriter stringWriter;
+        private TextWriter originalOutput;
+
+        public StreamOutput()
+        {
+            stringWriter = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+        }
+
+        public string GetOuput()
+        {
+            return stringWriter.ToString();
+        }
+
+        public void Flush()
+        {
+            Console.SetOut(originalOutput);
+            stringWriter.Dispose();
+        }
+    }
+}
