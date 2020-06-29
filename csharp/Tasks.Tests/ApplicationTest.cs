5a62c490869096ef33f00dcf9fb6f66088c8936c
Tholgan <marty.florian@gmail.com>
2020-06-29 17:04:44 +0200

    Trying to debug tests

--- csharp/Tasks.Tests/ApplicationTest.cs
+++ csharp/Tasks.Tests/ApplicationTest.cs
@@ -16,7 +16,7 @@ namespace Tasks
 		public void StartTheApplication()
 		{
 			this.console = new FakeConsole();
-			var taskList = new TaskList(console);
+			var taskList = new TaskList();
 			this.applicationThread = new System.Threading.Thread(() => taskList.Run());
 			applicationThread.Start();
 		}
